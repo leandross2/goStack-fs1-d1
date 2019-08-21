6d9b17c4d5ac824e2edd3c57c171961ec920c6ac
leandross2 <ls-saraiva2@hotmail.com>
2019-08-21 01:32:36 -0300

    add tasks

--- index.js
+++ index.js
@@ -33,11 +33,32 @@ server.put('/projects/:id', (req, res) => {
   const id = req.params.id
   projects.find((project, index) => {
     if (project.id == id) {
-      // projects[index].title = req.body.title;
+      projects[index].title = req.body.title
     }
     console.log('ok')
-    res.send('ok')
   })
+  res.json(projects)
+})
+server.delete('/projects/:id', (req, res) => {
+  const id = req.params.id
+  projects.find((project, index) => {
+    if (project.id == id) {
+      projects.splice(index, 1)
+    }
+  })
+  res.json(projects)
+})
+
+server.post('/projects/:id/tasks', (req, res) => {
+  const id = req.params.id
+  const newTasks = req.body.tasks
+  projects.find((project, index) => {
+    if (project.id == id) {
+      // console.log(projects[index].tasks.push('s'))
+      projects[index].tasks.concat(newTasks)
+    }
+  })
+  res.json(projects)
 })
 
 server.listen(3333)
